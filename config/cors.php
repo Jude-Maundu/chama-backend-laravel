05c3935a37bd479b4151c640d54cd6f26bda6815
Jude-Maundu <judemaundu001@gmail.com>
2026-05-06 15:31:35 +0300

    hadi sijui nimefanya nini 2

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,9 @@
+<?php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://chama-frontend-ph8d.onrender.com'],  // or specify your frontend URL
+    'allowed_origins' => ['https://chama-frontend-ph8d.onrender.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
